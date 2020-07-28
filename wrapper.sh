bd4bad6c64a0e58730b16819bb96bdcc33648fd9
Shane Meyers <shane@smugmug.com>
2020-07-28 23:30:34 +0000

    fix container name in wrapper

--- wrapper.sh
+++ wrapper.sh
@@ -2,6 +2,6 @@
 
 TMP=`mktemp`
 # not sure why, but docker is outputting DOS line endings
-docker run --rm -it -v $HOME/.aws:/root/.aws aws-role-login:latest $1 $2 | tr -d '\r' | tee $TMP
+docker run --rm -it -v $HOME/.aws:/root/.aws shanemeyers/aws-role-login:latest $1 $2 | tr -d '\r' | tee $TMP
 source $TMP
 rm -f $TMP
