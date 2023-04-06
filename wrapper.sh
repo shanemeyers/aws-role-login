227602fcfd384b1dfdd631d119250c02ed43f6c1
Shane Meyers <shane@shanemeyers.com>
2023-04-06 11:11:47 -0700

    better error handling in wrapper

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 TMP=`mktemp`
+
 # not sure why, but docker is outputting DOS line endings
 docker run --rm -it -v $HOME/.aws:/root/.aws shanemeyers/aws-role-login:latest $1 $2 | tr -d '\r' | tee $TMP
-source $TMP
+
+grep -q '^export' $TMP
+if [ $? = 0 ] ; then
+    source $TMP
+fi
 rm -f $TMP
